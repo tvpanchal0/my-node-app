6b7424e1f3a54ed60f9f66b33bbcdccf1cde61ed
programmer.tv <tusharpanchal740@gmail.com>
2025-01-07 10:34:55 +0530

    added name for reg

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  name:{ type: String},
   password: { type: String, required: true },
 });
 
